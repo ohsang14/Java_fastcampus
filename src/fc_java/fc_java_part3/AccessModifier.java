2456b5421018baa3c5dbb93dfad5cd5a04dd577d
ohsang <ast6108@naver.com>
2023-10-24 22:41:13 +0900

    commit

---
+++ src/fc_java/fc_java_part3/AccessModifier.java
@@ -0,0 +1,12 @@
+package fc_java.fc_java_part3;
+
+import fc_java.model.MemberDTO;
+
+public class AccessModifier {
+    public static void main(String[] args) {
+        MemberDTO dto = new MemberDTO();
+        dto.name = "길동이";
+        dto.phone = "010-2746-0094";
+
+    }
+}
---
+++ src/fc_java/model/MemberDTO.java
@@ -0,0 +1,8 @@
+package fc_java.model;
+
+public class MemberDTO {
+    public String name;
+    private int age;
+    public String phone;
+
+}
