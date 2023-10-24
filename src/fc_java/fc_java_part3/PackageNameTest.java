c3018152a05bc8c92dbc03995aee69abe6c64edc
ohsang <ast6108@naver.com>
2023-10-24 23:00:36 +0900

    commit

---
+++ src/fc_java/fc_java_part3/PackageNameTest.java
@@ -0,0 +1,14 @@
+package fc_java.fc_java_part3;
+
+import java.util.Scanner;
+
+public class PackageNameTest {
+    public static void main(String[] args) {
+        //Q. 지금까지 자바에서 제공해주어서 사용해본 클래스들의 이름을 적어보자?
+        String str =" 옥슬이";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello world");
+
+
+    }
+}
